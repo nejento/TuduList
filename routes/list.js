2453d924d7bf71640039ee44bbfc4413e23c7f5b
nejento <nejento@nejento.eu>
2019-04-30 11:31:06 +0200

    Přidávání a synchronizace položek

--- routes/list.js
+++ routes/list.js
@@ -1,25 +1,33 @@
 const express = require('express');
 let router = express.Router();
 
+//TODO: Přidat knihovnu XSS, která automaticky odstraní XSS skripty
+
+let polozky = [
+    { id: 1, name: "bob" },
+    { id: 2, name: "john" },
+    { id: 3, name: "jake" }
+];
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
     //res.send('respond with a resource');
     res.render('list', {
         title: 'Seznam',
-        polozky: [
-            { id: 1, name: "bob" },
-            { id: 2, name: "john" },
-            { id: 3, name: "jake" },
-        ]
+        polozky: polozky
     });
 });
 
 router.post('/', (req, res) => {
-    res.send('Got a POST request')
+    polozky.push({id: Math.max(...polozky.map(p => p.id)) + 1, name: req.body.task});
+    res.send(req.body.task);
+    console.log(req.body.task);
 });
 
+/*
 router.delete('/', (req, res) => {
     res.send('Got a DELETE request');
 });
+*/
 
 module.exports = router;
