f6e2d7f1ab6f37a3561b6fd55e7d6e2c4e3e2144
nejento <nejento@nejento.eu>
2019-05-03 12:54:24 +0200

    Bugfix

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,19 @@
 const express = require('express');
 let router = express.Router();
 
+/*
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'phplogin'
 });
-
+ */
 
 router.get('/', (req, res, next) => {
-    res.render('list', {
-        title: 'Seznam',
-        polozky: polozky
+    res.render('auth', {
+        title: 'Seznam'
     });
 });
 
-
+module.exports = router;
