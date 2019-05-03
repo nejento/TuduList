6dcb943ed410b353d1e6500e692ebbadeed9e400
nejento <nejento@nejento.eu>
2019-05-03 02:34:00 +0200

    Added serverside check

--- routes/list.js
+++ routes/list.js
@@ -23,20 +23,26 @@ router.post('/add', (req, res) => {
     if (req.body.task.trim().length <= 0) {
         res.status(406);
         res.send('Přidávaná položka nesmí mít nulovou velikost');
+    } else {
+        let maxID = Math.max(...polozky.map(p => p.id));
+        let newID = (maxID < 0 ? 0 : maxID) + 1;
+        console.log(`Adding task with ID: ${newID} with text: ${req.body.task}`);
+        polozky.push({id: newID, task: req.body.task.trim()});
+        res.send({id: "task-" + newID, task: req.body.task.trim()});
     }
-    let maxID = Math.max(...polozky.map(p => p.id));
-    let newID = (maxID < 0 ? 0 : maxID) + 1;
-    console.log(`Adding task with ID: ${newID} with text: ${req.body.task}`);
-    polozky.push({id: newID, task: req.body.task.trim()});
-    res.send({id: "task-" + newID, task: req.body.task.trim()});
 });
 
 //Editace položky
 router.put('/edit', (req, res) => {
-    let taskToEdit = polozky.findIndex((p => p.id === parseInt(req.body.id.replace("task-", ""))));
-    console.log(`Editing task ID: ${taskToEdit} with text: ${req.body.task}`);
-    polozky[taskToEdit].task = req.body.task;
-    res.send({task: polozky[taskToEdit].task});
+    if (req.body.task.trim().length <= 0) {
+        res.status(406);
+        res.send('Upravovaná položka nesmí mít nulovou velikost');
+    } else {
+        let taskToEdit = polozky.findIndex((p => p.id === parseInt(req.body.id.replace("task-", ""))));
+        console.log(`Editing task ID: ${taskToEdit} with text: ${req.body.task}`);
+        polozky[taskToEdit].task = req.body.task;
+        res.send({task: polozky[taskToEdit].task});
+    }
 });
 
 //Odstranění položky
